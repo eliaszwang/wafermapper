6278f27558cab0b68bbd24a8ef446ca4eb1e8aa4
Eli <ewang921@gmail.com>
2016-06-20 17:59:35 -0400

    fixed Kx,Ky actually...

--- WaferMapper_1.88/ACQUIRE/MAP.m
+++ WaferMapper_1.88/ACQUIRE/MAP.m
@@ -16,7 +16,7 @@ NA= 0.752 / (PixSize* (Acc*1000)^0.5); %confirm units for PixSize
 sigma =mean([std(double(I1(:))), std(double(I2(:)))]);
 %cutoffx=int32(floor(0.125*width)); %cutoff for k's used based on 25% k_nyquist, cycles/pixel
 %cutoffy=int32(floor(0.125*height)); %use for selecting subset of I/K e.g. K([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end])
-[Kx, Ky]=meshgrid([0:width-1]/width*(6.28/FOV),[0:height-1]/height*(6.28/FOV)); %units are rad/um?
+[Kx, Ky]=meshgrid((circshift([0:width-1]/width,width/2,2)-0.5)*(6.28/FOV),(circshift([0:width-1]/width,width/2,2)-0.5)*(6.28/FOV)); %units are rad/um?
 %Kx=[1:width]*(6.28/FOV);
 %Ky=[1:height]*(6.28/FOV);
 
@@ -24,7 +24,7 @@ sigma =mean([std(double(I1(:))), std(double(I2(:)))]);
 p_A=@(A)  (max([abs(A(1)),abs(A(2)),abs(A(3))])<=A_max)/((2*A_max)^3); %uniform distribtion over cube of side 2*A_max
 %MTF=@(kx,ky,A) exp(-0.125*NA^2*(2*A(2)*(kx^2-ky^2)*A(1)-A(3)*kx*ky*A(1)+(kx^2+ky^2)*(A(1)^2+A(2)^2+A(3)^2))); %check for typos
 MTF=@(Kx,Ky,A) exp(-0.125*(NA^2)*(2*A(2)*(Kx.^2 - Ky.^2)*A(1) - A(3)*Kx.*Ky*A(1) + (Kx.^2+Ky.^2).*(A(2)^2 + A(3)^2 + A(1)^2)));
-f=-log(p_A(A))+sum(sum( ((fI2.*MTF(Kx,Ky,A+T1) - fI1.*MTF(Kx,Ky,A+T2)).^2) ./ (2*sigma^2*(MTF(Kx,Ky,A+T1).^2+MTF(Kx,Ky,A+T2).^2)+1e-10) ));
+f=-log(p_A(A))+sum(sum( (abs(fI2.*MTF(Kx,Ky,A+T1) - fI1.*MTF(Kx,Ky,A+T2)).^2) ./ (2*sigma^2*(MTF(Kx,Ky,A+T1).^2+MTF(Kx,Ky,A+T2).^2)+1e-10) ));
 
 %MTF equation used to calculate partial derivatives in Wolfram
 %exp(-0.125*(N^2)*(2a*(x^2-y^2)*z-b*x*y*z+(x^2+y^2)(a^2+b^2+z^2)))
