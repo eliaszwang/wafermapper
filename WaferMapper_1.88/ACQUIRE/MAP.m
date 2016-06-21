618a7bf4441c59eb54bbc7e023c219ca0306ed59
Eli <ewang921@gmail.com>
2016-06-21 17:55:35 -0400

    single aberration should work, still testing multiple modes

--- WaferMapper_1.88/ACQUIRE/MAP.m
+++ WaferMapper_1.88/ACQUIRE/MAP.m
@@ -12,23 +12,28 @@ PixSize = FOV/height; % um per pixel
 A_max=80; %set max defocus and astigmatism to 80um-based of paper, needs to be changed
 %NA=5; %set NA to 5 mrad-based of paper, needs to be changed
 NA= 0.752 / (PixSize* (Acc*1000)^0.5); %confirm units for PixSize
-%sigma=1; %estimated Gaussian noise (approximation for shot noise), rad/um (maybe calculate later)
-sigma =mean([std(double(I1(:))), std(double(I2(:)))]);
-%cutoffx=int32(floor(0.125*width)); %cutoff for k's used based on 25% k_nyquist, cycles/pixel
-%cutoffy=int32(floor(0.125*height)); %use for selecting subset of I/K e.g. K([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end])
+sigma=10; %estimated Gaussian noise (approximation for shot noise), rad/um (maybe calculate later)
+%sigma =mean([std(double(I1(:))), std(double(I2(:)))]);
+cutoffx=int32(floor(0.125*width)); %cutoff for k's used based on 25% k_nyquist, cycles/pixel
+cutoffy=int32(floor(0.125*height)); %use for selecting subset of I/K e.g. K([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end])
 [Kx, Ky]=meshgrid((circshift([0:width-1]/width,width/2,2)-0.5)*(6.28/FOV),(circshift([0:width-1]/width,width/2,2)-0.5)*(6.28/FOV)); %units are rad/um?
-%Kx=[1:width]*(6.28/FOV);
-%Ky=[1:height]*(6.28/FOV);
 
-%{
+Kx=Kx([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
+Ky=Ky([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
+fI1=fI1([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
+fI2=fI2([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
+% Kx=Kx(1,:);
+% Ky=Ky(1,:);
+% fI1=fI1(1,:);
+% fI2=fI2(1,:);
 Kx2=Kx.^2;
 Ky2=Ky.^2;
-MTF1=MTF(Kx,Ky,A+T1);
-MTF2=MTF(Kx,Ky,A+T2);
+
 %% Setup probability functions for MAPFoSt
 p_A=@(A)  (max([abs(A(1)),abs(A(2)),abs(A(3))])<=A_max)/((2*A_max)^3); %uniform distribtion over cube of side 2*A_max
 MTF=@(Kx,Ky,A) exp(-0.125*(NA^2)*(2*A(2)*(Kx2 - Ky2)*A(1) - A(3)*Kx.*Ky*A(1) + (Kx2+Ky2)*(A(2)^2 + A(3)^2 + A(1)^2)));
-
+MTF1=MTF(Kx,Ky,A+T1);
+MTF2=MTF(Kx,Ky,A+T2);
 %MTF equation used to calculate partial derivatives in Wolfram
 %exp(-0.125*(N^2)*(2a*(x^2-y^2)*z-b*x*y*z+(x^2+y^2)(a^2+b^2+z^2)))
 dMTFd1=@(Kx,Ky,A) -0.125*(NA^2)*(2*A(2)*(Kx2 - Ky2) + 2*A(1)*(Kx2 + Ky2) - A(3)*Kx.*Ky).*exp(-0.125*(NA^2)*(2*A(2)*(Kx2 - Ky2)*A(1) - A(3)*Kx.*Ky*A(1) + (Kx2+Ky2)*(A(2)^2 + A(3)^2 + A(1)^2)));
@@ -39,16 +44,11 @@ dMTFd3=@(Kx,Ky,A) -0.125*(NA^2)*(2*A(3)*(Kx2 + Ky2) - A(1)*Kx.*Ky).*exp(-0.125*(
 df(1)= sum(sum( (2*(MTF1.*dMTFd1(Kx,Ky,A+T2)-MTF2.*dMTFd1(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF1.^2+MTF2.^2).^2) ));
 df(2)= sum(sum( (2*(MTF1.*dMTFd2(Kx,Ky,A+T2)-MTF2.*dMTFd2(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF1.^2+MTF2.^2).^2) ));
 df(3)= sum(sum( (2*(MTF1.*dMTFd3(Kx,Ky,A+T2)-MTF2.*dMTFd3(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF1.^2+MTF2.^2).^2) ));
-f=-log(p_A(A))+sum(sum( (abs(fI2.*MTF1 - fI1.*MTF2).^2) ./ (2*sigma^2*(MTF1.^2+MTF2.^2)) ));
+f=-log(p_A(A))+sum(sum( ((fI2.*MTF1 - fI1.*MTF2).^2) ./ (2*sigma^2*(MTF1.^2+MTF2.^2)) ));
 %f=sum(sum( ((fI2.*MTF1 - fI1.*MTF2).^2) ./ (2*sigma^2*(MTF1.^2+MTF2.^2)) ));
-%}
-Kx=Kx(1,:);
-Ky=Ky(1,:);
-fI1=fI1(1,:);
-fI2=fI2(1,:);
+
+%{
 %for single aberration mode
-Kx2=Kx.^2;
-Ky2=Ky.^2;
 Kx2Ky2=Kx2+Ky2;
 p_A=@(A)  (max(abs(A))<=A_max)/(2*A_max); %uniform distribtion over cube of side 2*A_max
 MTF=@(Kx,Ky,A) exp(-0.125*(NA^2)*(Kx2Ky2)*A^2);
@@ -57,5 +57,5 @@ MTF1=MTF(Kx,Ky,A+T1);
 MTF2=MTF(Kx,Ky,A+T2);
 df=sum(sum( (2*(MTF1.*dMTFdz(Kx,Ky,A+T2)-MTF2.*dMTFdz(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF1.^2+MTF2.^2).^2) ));
 f=-log(p_A(A))+sum(sum( ((fI2.*MTF1 - fI1.*MTF2).^2) ./ (2*sigma^2*(MTF1.^2+MTF2.^2)) ));
-
+%}
 end
--- WaferMapper_1.88/ACQUIRE/MAPFoSt_simulation.m
+++ WaferMapper_1.88/ACQUIRE/MAPFoSt_simulation.m
@@ -12,11 +12,12 @@ mtf=fft2(psf);
 
 tic;
 out=[];
-for i=1:40
+r=1:20;
+for i=r
 I1=raw;
 I2=raw;
 %test initial aberration
-A=[i];
+A=[i i/2 i/3];
 
 %% Initialize/calculate constants
 %Calculate fft of two images
@@ -29,25 +30,23 @@ Acc=5;
 PixSize = FOV/height; % um per pixel
 A_max=80; %set max defocus and astigmatism to 80um-based of paper, needs to be changed
 NA= 0.752 / (PixSize* (Acc*1000)^0.5);
-%sigma=1; %estimated Gaussian noise (approximation for shot noise), rad/um (maybe calculate later)
-sigma =mean([std(double(I1(:))), std(double(I2(:)))]); %sigma for real space
+sigma=10; %estimated Gaussian noise (approximation for shot noise), rad/um (maybe calculate later)
+%sigma =mean([std(double(I1(:))), std(double(I2(:)))]); %sigma for real space
 cutoffx=int32(floor(0.125*width)); %cutoff for k's used based on 25% k_nyquist, cycles/pixel
 cutoffy=int32(floor(0.125*height)); %use for selecting subset of I/K e.g. K([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end])
 %[Kx, Ky]=meshgrid((mod(0.5+[0:width-1]/width,1)-0.5)*(6.28/FOV),(mod(0.5+[0:height-1]/height,1)-0.5)*(6.28/FOV)); %units are rad/um?
 [Kx, Ky]=meshgrid((circshift([0:width-1]/width,width/2,2)-0.5)*(6.28/FOV),(circshift([0:width-1]/width,width/2,2)-0.5)*(6.28/FOV)); %units are rad/um?
 
-%[Kx,Ky] = meshgrid ([1:width]/(PixSize*width*1e-6),[1:height]/(PixSize*height*1e-6) );
-%Kx=[1:width]*(6.28/FOV);
-%Ky=[1:height]*(6.28/FOV);
-%MTF=@(Kx,Ky,A) exp(-0.125*(NA^2)*(2*A(2)*(Kx.^2 - Ky.^2)*A(1) - A(3)*Kx.*Ky*A(1) + (Kx.^2+Ky.^2)*(A(2)^2 + A(3)^2 + A(1)^2)));
 
- MTF=@(Kx,Ky,A) exp(-0.125*(NA^2)*(Kx.^2+Ky.^2)*A^2);
+MTF=@(Kx,Ky,A) exp(-0.125*(NA^2)*(2*A(2)*(Kx.^2 - Ky.^2)*A(1) - A(3)*Kx.*Ky*A(1) + (Kx.^2+Ky.^2)*(A(2)^2 + A(3)^2 + A(1)^2)));
+
+ %MTF=@(Kx,Ky,A) exp(-0.125*(NA^2)*(Kx.^2+Ky.^2)*A^2);
 
 % hardcoded test aberrations (defocus only)
-T1=[15 ]; %defocus in [um]
-T2=[-15 ];
+T1=[15 0 0]; %defocus in [um]
+T2=[-15 0 0];
 
-noise=fft2(normrnd(0,sigma,height,width))+noise;
+noise=fft2(normrnd(0,sigma,height,width));
 fI1=fI1.*MTF(Kx,Ky,A+T1);
 %fI1=fI1.*mtf;
 noise=fft2(normrnd(0,sigma,height,width));
@@ -61,7 +60,7 @@ I2=(ifft2(fI2));
 % figure;
 % imshow([fftshift(fI1) fftshift(fI2)]);
 % figure;
-% imshow([I1 I2]);
+% imshow([uint8(255*I1/max(max(I1))) uint8(255*I2/max(max(I2)))]);
 % 
 % fOmap=(fI1.*MTF(Kx,Ky,A+T1)+fI2.*MTF(Kx,Ky,A+T2))./(MTF(Kx,Ky,A+T1).^2+MTF(Kx,Ky,A+T2).^2);
 % Omap=ifft2(fOmap);
@@ -71,12 +70,31 @@ I2=(ifft2(fI2));
 % 
 % MAP(A,I1,I2,T1,T2,FOV,Acc);
 % checkgrad('MAP', randn(1,1), 1e-5,I1,I2,T1',T2',FOV,Acc);
+p.length=10;
+p.method='BFGS';
+p.verbosity=0;
+p.MFEPLS = 10;   % Max Func Evals Per Line Search
+p.MSR = 100;                % Max Slope Ratio default
+O=minimize([0 0 0],@MAP,p,I1,I2,T1,T2,FOV,Acc);
+out=[out [O';MAP(A,I1,I2,T1,T2,FOV,Acc)-MAP(O,I1,I2,T1,T2,FOV,Acc)]];
 
-O=minimize([0],@MAP,5,I1,I2,T1,T2,FOV,Acc);
-out=[out O];
 end
 toc;
-plot(1:20,out,1:20,1:20,':');
+close all
+plot(r,(out(1,:)),r,r,':');
+title('estimated defocus vs actual');
+xlabel('actual');
+ylabel('estimated');
+figure;
+plot(r,(out(2,:)),r,r/2,':');
+title('estimated aon vs actual');
+xlabel('actual');
+ylabel('estimated');
+figure;
+plot(r,(out(3,:)),r,r/3,':');
+title('estimated adiag vs actual');
+xlabel('actual');
+ylabel('estimated');
 % set new WD/Stig from algorithm
 % z=O(1);
 % a_on=O(2);
