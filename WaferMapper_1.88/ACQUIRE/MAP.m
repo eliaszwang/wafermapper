ec1d076d6c7bdb600451ef006e30afe4e674d922
eli <ewang921@gmail.com>
2016-08-03 11:14:46 -0400

    cleaned up code

--- WaferMapper_1.88/ACQUIRE/MAP.m
+++ WaferMapper_1.88/ACQUIRE/MAP.m
@@ -1,106 +1,45 @@
 function [f, df]=MAP(A,fI1,fI2,T1,T2,NA,sigma,Kx, Ky)
-%MAP function to maximize with respect to aberration vector, A
-%T1 and T2 are the known test aberrations
-gaussian =0;
+%   MAP function to maximize with respect to aberration vector, A
+%   fI1,fI2: fft2 of the images
+%   T1, T2: known test aberrations
+%   NA: numerical aperature (empirically estimated)
+%   sigma: image noise std
+%   Kx,Ky: wave vector (meshgrid)
+
+gaussian =0; %gaussian prior flag
+
 %% Initialize/calculate constants
-%Calculate fft of two images
 height=size(fI1,1);
 width=size(fI1,2);
-%PixSize = FOV/height; % um per pixel
-A_max=50; %set max defocus and astigmatism to 50um
+A_max=50; %set max defocus to 50um
 A_sigma=20; %sigma for gaussian prior
-%NA=5; %set NA to 5 mrad-based of paper, needs to be changed
-%NA= 0.752 / (PixSize* (Acc*1000)^0.5); %confirm units for PixSize
 NA2=NA^2;
-%sigma=10; %estimated Gaussian noise (approximation for shot noise), rad/um (maybe calculate later)
-%sigma =mean([std(double(I1(:))), std(double(I2(:)))]);
-%[Kx, Ky]=meshgrid((mod(0.5+[0:width-1]/width,1)-0.5)*(6.28/FOV),(mod(0.5+[0:height-1]/height,1)-0.5)*(6.28/FOV));
-% determine which wave vectors to use
-% Kx=Kx([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-% Ky=Ky([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-% fI1=fI1([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-% fI2=fI2([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-% Kx=Kx(1,:);
-% Ky=Ky(1,:);
-% fI1=fI1(1,:);
-% fI2=fI2(1,:);
+cutoffx=int32(floor(0.25*width)); %cutoff for k's used based on 50% k_nyquist, cycles/pixel
+cutoffy=int32(floor(0.25*height)); %use for selecting subset of I/K e.g. K([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end])
+%% select which wave vectors to use (based on cutoff)
+Kx=Kx([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
+Ky=Ky([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
+fI1=fI1([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
+fI2=fI2([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
+%% precompute some matrices
+Kx2=Kx.^2;
+Ky2=Ky.^2;
+Kx2Ky2=Kx2+Ky2;
+p_A=@(A)  (max(abs(A))<=A_max)/(2*A_max); % define uniform distribtion over line of length 2*A_max
+MTF=@(Kx,Ky,A) exp(-0.125*(NA2)*(Kx2Ky2)*A^2); % define gaussian MTF
+dMTFdz=@(Kx,Ky,A) -0.25*(NA2)*(Kx2Ky2)*A.*exp(-0.125*(NA2)*(Kx2Ky2)*A^2); % define derivative of MTF
+MTF1=MTF(Kx,Ky,A+T1);
+MTF2=MTF(Kx,Ky,A+T2);
+MTF12=MTF1.^2;
+MTF22=MTF2.^2;
 
+likelihood=((fI2.*MTF1 - fI1.*MTF2).^2) ./ (2*sigma^2*(MTF12+MTF22)+1e-20); %this is the matrix of log likelihoods (ie the exponentiated part of eq 27)
 
-if single %for single aberration mode
-    cutoffx=int32(floor(0.25*width)); %cutoff for k's used based on 50% k_nyquist, cycles/pixel
-    cutoffy=int32(floor(0.25*height)); %use for selecting subset of I/K e.g. K([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end])
-    % determine which wave vectors to use
-    Kx=Kx([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-    Ky=Ky([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-    fI1=fI1([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-    fI2=fI2([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-    %precompute some matrices
-    Kx2=Kx.^2;
-    Ky2=Ky.^2;
-    Kx2Ky2=Kx2+Ky2;
-    p_A=@(A)  (max(abs(A))<=A_max)/(2*A_max); %uniform distribtion over line of length 2*A_max
-    MTF=@(Kx,Ky,A) exp(-0.125*(NA2)*(Kx2Ky2)*A^2);
-    MTF1=MTF(Kx,Ky,A+T1);
-    MTF2=MTF(Kx,Ky,A+T2);
-    MTF12=MTF1.^2;
-    MTF22=MTF2.^2;
-    likelihood=((fI2.*MTF1 - fI1.*MTF2).^2) ./ (2*sigma^2*(MTF12+MTF22)+1e-20); %this is the matrix of log likelihoods (ie the exponentiated part of eq 27)
-    dMTFdz=@(Kx,Ky,A) -0.25*(NA2)*(Kx2Ky2)*A.*exp(-0.125*(NA2)*(Kx2Ky2)*A^2);
-    %df=sum(sum( (2*(fI2.*MTF1-fI1.*MTF2).*(fI2.*dMTFdz(Kx,Ky,A+T1)-fI1.*dMTFdz(Kx,Ky,A+T2)))./(2*sigma^2*(MTF1.^2+MTF2.^2)+1e-20)-(2*(MTF1.*dMTFdz(Kx,Ky,A+T1)+MTF2.*dMTFdz(Kx,Ky,A+T2)).*(fI2.*MTF1-fI1.*MTF2).^2)./(2*sigma^2*(MTF1.^2+MTF2.^2).^2+1e-20) ));
-    df=(2*(MTF1.*dMTFdz(Kx,Ky,A+T2)-MTF2.*dMTFdz(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF12+MTF22).^2+1e-20) ;
-    df=sum(sum( df(likelihood>0) )); %only take the df terms from the ones we're using to calculate f
-    if gaussian
-        p_A=@(A)  (1/(2.5066*A_sigma))*exp(-A^2/(2*A_sigma^2)); %gaussian prior
-        df=df+A/A_sigma^2;
-    end
-    f=-log(p_A(A))+sum(sum( likelihood(likelihood>0) ));
-else %3 modes
-%     cutoffx=int32(floor(0.35*width)); %cutoff for k's used based on 50% k_nyquist, cycles/pixel
-%     cutoffy=int32(floor(0.35*height)); %use for selecting subset of I/K e.g. K([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end])
-%     % determine which wave vectors to use
-%     Kx=Kx([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-%     Ky=Ky([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-%     fI1=fI1([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-%     fI2=fI2([1:cutoffy end+1-cutoffy:end],[1:cutoffx end+1-cutoffx:end]);
-    %precompute some matrices
-    Kx2=Kx.^2;
-    Ky2=Ky.^2;
-    % Setup probability functions for MAPFoSt
-    p_A=@(A)  (max([abs(A(1)),abs(A(2)),abs(A(3))])<=A_max)/((2*A_max)^3); %uniform distribtion over cube of side 2*A_max
-    MTF=@(Kx,Ky,A) exp(-0.125*(NA2)*(-2*A(2)*(Kx2 - Ky2)*A(1) - 4*A(3)*Kx.*Ky*A(1) + (Kx2 + Ky2)*(A(2)^2 + A(3)^2 + A(1)^2)));
-    MTF1=MTF(Kx,Ky,A+T1);
-    MTF2=MTF(Kx,Ky,A+T2);
-    MTF12=MTF1.^2;
-    MTF22=MTF2.^2;
-    likelihood=((fI2.*MTF1 - fI1.*MTF2).^2) ./ (2*sigma^2*(MTF12+MTF22)+1e-20); %this is the matrix of log likelihoods (ie the exponentiated part of eq 27)
-    Kx=Kx(likelihood>0);
-    Ky=Ky(likelihood>0);
-    fI1=fI1(likelihood>0);
-    fI2=fI2(likelihood>0);
-    KxKy=Kx.*Ky;
-    Kx2=Kx.^2;
-    Ky2=Ky.^2;
-    Kx2Ky2=Kx2 + Ky2;
-    MTF=@(Kx,Ky,A) exp(-0.125*(NA2)*(-2*A(2)*(Kx2 - Ky2)*A(1) - 4*A(3)*Kx.*Ky*A(1) + (Kx2 + Ky2)*(A(2)^2 + A(3)^2 + A(1)^2)));
-    MTF1=MTF(Kx,Ky,A+T1);
-    MTF2=MTF(Kx,Ky,A+T2);
-    MTF12=MTF1.^2;
-    MTF22=MTF2.^2;
-    %MTF equation used to calculate partial derivatives in Wolfram
-    %exp(-0.125*(N^2)*(2a*(x^2-y^2)*z-b*x*y*z+(x^2+y^2)(a^2+b^2+z^2)))
-    dMTFd1=@(Kx,Ky,A) -0.125*(NA2)*(-2*A(2)*(Kx2 - Ky2) + 2*A(1)*(Kx2Ky2) - 4*A(3)*KxKy).*exp(-0.125*(NA2)*(-2*A(2)*(Kx2 - Ky2)*A(1) - 4*A(3)*KxKy*A(1) + (Kx2Ky2)*(A(2)^2 + A(3)^2 + A(1)^2)));
-    dMTFd2=@(Kx,Ky,A) -0.125*(NA2)*(2*A(2)*(Kx2Ky2) - 2*A(1)*(Kx2 - Ky2)).*exp(-0.125*(NA2)*(-2*A(2)*(Kx2 - Ky2)*A(1) - 4*A(3)*KxKy*A(1) + (Kx2Ky2)*(A(2)^2 + A(3)^2 + A(1)^2)));
-    dMTFd3=@(Kx,Ky,A) -0.125*(NA2)*(2*A(3)*(Kx2Ky2) - 4*A(1)*KxKy).*exp(-0.125*(NA2)*(-2*A(2)*(Kx.^2 - Ky.^2)*A(1) - 4*A(3)*KxKy*A(1) + (Kx2Ky2)*(A(2)^2 + A(3)^2 + A(1)^2)));
-
-    % d/dx (a*f(x+i)-b*f(x+j))^2/(c*(f(x+i)^2+f(x+j)^2))
-    % d/dx (((a+c*i)*f(x+g)-(b+d*i)*f(x+h))^2)/(s*(f(x+g)^2+f(x+h)^2))
-    temp=(2*(MTF1.*dMTFd1(Kx,Ky,A+T2)-MTF2.*dMTFd1(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF12+MTF22).^2+1e-20);
-    df(1)= sum(sum( temp ));
-    temp=(2*(MTF1.*dMTFd2(Kx,Ky,A+T2)-MTF2.*dMTFd2(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF12+MTF22).^2+1e-20);
-    df(2)= sum(sum( temp ));
-    temp=(2*(MTF1.*dMTFd3(Kx,Ky,A+T2)-MTF2.*dMTFd3(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF12+MTF22).^2+1e-20);
-    df(3)= sum(sum( temp ));
-    f=-log(p_A(A))+sum(sum( likelihood(likelihood>0) ));
-    %f=sum(sum( ((fI2.*MTF1 - fI1.*MTF2).^2) ./ (2*sigma^2*(MTF1.^2+MTF2.^2)) ));
+df=(2*(MTF1.*dMTFdz(Kx,Ky,A+T2)-MTF2.*dMTFdz(Kx,Ky,A+T1)).*(fI2.*MTF2+fI1.*MTF1).*(fI1.*MTF2-fI2.*MTF1))./(2*sigma^2*(MTF12+MTF22).^2+1e-20) ;
+df=sum(sum( df(likelihood>0) )); %only take the df terms from the ones we're using to calculate f
+if gaussian
+    p_A=@(A)  (1/(2.5066*A_sigma))*exp(-A^2/(2*A_sigma^2)); % define gaussian prior
+    df=df+A/A_sigma^2;
 end
+f=-log(p_A(A))+sum(sum( likelihood(likelihood>0) )); %take positive log likelihoods
 end
--- WaferMapper_1.88/ACQUIRE/MAPFoSt.m
+++ WaferMapper_1.88/ACQUIRE/MAPFoSt.m
@@ -1,46 +1,52 @@
-function [z,finalWD,I1,I2]=MAPFoSt(ImageHeightInPixels,ImageWidthInPixels,DwellTimeInMicroseconds,FileName,FOV,maxiter,single)
-% Elias Wang's implementation of MAPFoSt
-% algorithm adapted from Jonas Binding (Low-Dosage Maximum-A-Posteriori Focusing and Stigmation)
-% should replace "sm.Execute('CMD_AUTO_FOCUS_FINE')" calls
-global GuiGlobalsStruct;
+function [z,finalWD,I1,I2]=MAPFoSt(ImageHeightInPixels,ImageWidthInPixels,DwellTimeInMicroseconds,FileName,FOV,maxiter,fallback,verbosity)
+%   Elias Wang's implementation of MAPFoSt
+%   Adapted from Jonas Binding (Low-Dosage Maximum-A-Posteriori Focusing and Stigmation)
+%   Should replace "sm.Execute('CMD_AUTO_FOCUS_FINE')" calls
+%   *INPUTS:
+%   ImageHeightInPixels, ImageWidthInPixels: desired dimension of test images
+%   DwellTimeInMicroseconds: test image dwell time
+%   FileName: temp filename for saved images
+%   FOV: image Field of View
+%   maxiter: max number of iterations if algorithm fails
+%   fallback: default to Zeiss autofocus after maxiter
+%   *OUTPUTS
+%   z: (relative) aberration estimate, in um
+%   finalWD: vector of final WD and stigmation values set by algorithm
+%   I1,I2: two test images taken
+
+
+if ~exist('verbosity','var')
+    verbosity=0;
+end
+
+global GuiGlobalsStruct; % WaferMapper global variable
 sm = GuiGlobalsStruct.MyCZEMAPIClass; %To shorten calls to global API variables in this function
 
-% hardcoded test aberrations
+% hardcoded test aberrations, in um
 T1=15;
 T2=-15;
 
-
-
-frametime=sm.Get_ReturnTypeSingle('AP_FRAME_TIME')/1000;
-frametime=1;
+% set delay after changing WD
+%frametime=sm.Get_ReturnTypeSingle('AP_FRAME_TIME')/1000; %time in seconds to scan a whole frame
+frametime=1; 
 
 %begin MAPFoSt
 CurrentWorkingDistance = sm.Get_ReturnTypeSingle('AP_WD');
 CurrentStigX = sm.Get_ReturnTypeSingle('AP_STIG_X');
 CurrentStigY = sm.Get_ReturnTypeSingle('AP_STIG_Y');
-disp('Beginning MAPFoSt...');
-disp(['starting WD: ' num2str(10^6*CurrentWorkingDistance) 'um' num2str(CurrentStigX) ' ' num2str(CurrentStigY)]);
+if verbosity
+    disp('Beginning MAPFoSt...');
+    disp(['starting values: ' num2str(10^6*CurrentWorkingDistance) 'um, ' num2str(CurrentStigX) '%, ' num2str(CurrentStigY) '%']);
+end
 
 
-% %*** START: This sequence is designed to release the SEM from Fibics control
-%     sm.Execute('CMD_AUTO_FOCUS_FINE');
-%     pause(0.5);
-%     sm.Execute('CMD_ABORT_AUTO');
-%     while ~strcmp('Idle',sm.Get_ReturnTypeString('DP_AUTO_FUNCTION'))
-%         pause(0.02);
-%     end
-%     sm.Set_PassedTypeSingle('AP_WD',CurrentWorkingDistance);
-%     
-%     pause(0.1);
-   
-
-%%Take first image
-%implement errorcheck for test aberration
+%% Take first image
 sm.Set_PassedTypeSingle('AP_WD',CurrentWorkingDistance+10^-6*T1);
-sm.Set_PassedTypeSingle('AP_SCANROTATION',0);
 pause(frametime);
 T1WD=sm.Get_ReturnTypeSingle('AP_WD');
-disp(['first image WD: ' num2str(10^6*T1WD) 'um']);
+if verbosity
+    disp(['first image WD: ' num2str(10^6*T1WD) 'um']);
+end
 sm.Fibics_WriteFOV(FOV);
 %Wait for image to be acquired
 sm.Fibics_AcquireImage(ImageWidthInPixels,ImageHeightInPixels,DwellTimeInMicroseconds,FileName);
@@ -60,28 +66,16 @@ while ~IsReadOK
     end
 end
 
-delete(FileName);
-
-% %*** START: This sequence is designed to release the SEM from Fibics control
-%     sm.Execute('CMD_AUTO_FOCUS_FINE');
-%     pause(0.5);
-%     sm.Execute('CMD_ABORT_AUTO');
-%     while ~strcmp('Idle',sm.Get_ReturnTypeString('DP_AUTO_FUNCTION'))
-%         pause(0.02);
-%     end
-%     sm.Set_PassedTypeSingle('AP_WD',CurrentWorkingDistance);
-%     
-%     pause(0.1);
-%     
+delete(FileName); % for some reason, deleting image seemed to be necessary
 
 
-%%Take second image
-%implement errorcheck for test aberration
+%% Take second image
 sm.Set_PassedTypeSingle('AP_WD',CurrentWorkingDistance+10^-6*T2);
-sm.Set_PassedTypeSingle('AP_SCANROTATION',0);
 pause(frametime);
 T2WD=sm.Get_ReturnTypeSingle('AP_WD');
-disp(['second image WD: ' num2str(10^6*T2WD) 'um']);
+if verbosity
+    disp(['second image WD: ' num2str(10^6*T2WD) 'um']);
+end
 sm.Fibics_WriteFOV(FOV);
 %Wait for image to be acquired
 sm.Fibics_AcquireImage(ImageWidthInPixels,ImageHeightInPixels,DwellTimeInMicroseconds,FileName);
@@ -103,7 +97,8 @@ end
 
 delete(FileName);
 
-%*** START: This sequence is designed to release the SEM from Fibics control
+
+% *** START: This sequence is designed to release the SEM from Fibics control
     sm.Execute('CMD_AUTO_FOCUS_FINE');
     pause(0.5);
     sm.Execute('CMD_ABORT_AUTO');
@@ -111,64 +106,57 @@ delete(FileName);
         pause(0.02);
     end
     sm.Set_PassedTypeSingle('AP_WD',CurrentWorkingDistance);
-    
     pause(0.1);
+% *** END 
 
-%get actual aberrations
+%get actual (test) aberrations, in um
     T1=10^6*(T1WD-CurrentWorkingDistance);
     T2=10^6*(T2WD-CurrentWorkingDistance);
     
 %% MAPFoSt
-%PixSize = FOV/ImageHeightInPixels; % um per pixel
+% setup variables
 Acc=5;
-%NA= sqrt(40)*0.752 / (PixSize* (Acc*1000)^0.5);
-NA= 0.5596*ImageHeightInPixels / ((Acc*1000)^0.5); %empirically determined constant;
-sigma =mean([std(double(I1(:))), std(double(I2(:)))]); %sigma for real space
-[Kx, Ky]=meshgrid((mod(0.5+[0:ImageWidthInPixels-1]/ImageWidthInPixels,1)-0.5)*(6.28/FOV),(mod(0.5+[0:ImageHeightInPixels-1]/ImageHeightInPixels,1)-0.5)*(6.28/FOV));
+%NA= sqrt(40)*0.752 / (PixSize* (Acc*1000)^0.5); 
+NA= 0.5596*ImageHeightInPixels / ((Acc*1000)^0.5); % empirically determined constant;
+sigma =mean([std(double(I1(:))), std(double(I2(:)))]); % determine sigma for real space from images
+[Kx, Ky]=meshgrid((mod(0.5+[0:ImageWidthInPixels-1]/ImageWidthInPixels,1)-0.5)*(6.28/FOV),(mod(0.5+[0:ImageHeightInPixels-1]/ImageHeightInPixels,1)-0.5)*(6.28/FOV)); % calculate wave vectors
 fI1=fft2(double(I1)); %image should have dimension 2^n for faster FFT
-fI2=fft2(double(I2));
-if single
-    init=2;
-else
-    init=[2 2 2];
-end
+fI2=fft2(double(I2)); 
+init=0; % note: non-zero initialization seemed to work a bit better
 
-
-
+% minimize parameters
 p.length=20;
 p.method='BFGS';
-p.verbosity=1;
+p.verbosity=verbosity;
 p.MFEPLS = 30;   % Max Func Evals Per Line Search
 p.MSR = 100;     % Max Slope Ratio default
-O=minimize(init,@MAP,p,fI1,fI2,T1,T2,NA,sigma,Kx,Ky,single);
+% run minimization on 'MAP' function
+O=minimize(init,@MAP,p,fI1,fI2,T1,T2,NA,sigma,Kx,Ky);
 
 %%
-if max(abs(O))<20 %aberration estimate too large, assume incorrect
+if max(abs(O))<20 % make sure aberration estimate is reasonable (ie less than 20 um)
     % set new WD/Stig from algorithm
-    if single
-        sm.Set_PassedTypeSingle('AP_WD',CurrentWorkingDistance-10^-6*real(O)); %change WD
-    else
-        R=[-0.1245 0.0093;0.0584 0.0323]; %rotation matrix
-        X=R*real([O(2);O(3)]); %apply rotation matrix to estimated [aon;adiag]
-        sm.Set_PassedTypeSingle('AP_WD',CurrentWorkingDistance-10^-6*real(O(1))); %change WD 
-        sm.Set_PassedTypeSingle('AP_STIG_X',CurrentStigX-X(1)); 
-        sm.Set_PassedTypeSingle('AP_STIG_Y',CurrentStigY-X(2));
-    end
+    sm.Set_PassedTypeSingle('AP_WD',CurrentWorkingDistance-10^-6*real(O)); 
     finalWD=[sm.Get_ReturnTypeSingle('AP_WD') sm.Get_ReturnTypeSingle('AP_STIG_X') sm.Get_ReturnTypeSingle('AP_STIG_Y')];
-    disp(['final WD: ' num2str(10^6*finalWD(1)) 'um' num2str(finalWD(2)) ' ' num2str(finalWD(3))]);
+    if verbosity
+        disp(['final WD: ' num2str(10^6*finalWD(1)) 'um' num2str(finalWD(2)) ' ' num2str(finalWD(3))]);
+    end
     z=real(O);
 elseif maxiter>1
     [z,finalWD,I1,I2]=MAPFoSt(ImageHeightInPixels,ImageWidthInPixels,DwellTimeInMicroseconds,FileName,FOV,maxiter-1,single); 
 else
-%     sm.Execute('CMD_AUTO_FOCUS_FINE');
-%     pause(0.5);
-%     while ~strcmp('Idle',sm.Get_ReturnTypeString('DP_AUTO_FUNCTION'))
-%         pause(0.02);
-%     end
+    if fallback
+        sm.Execute('CMD_AUTO_FOCUS_FINE');
+        pause(0.5);
+        while ~strcmp('Idle',sm.Get_ReturnTypeString('DP_AUTO_FUNCTION'))
+            pause(0.02);
+        end
+    end
     finalWD=[sm.Get_ReturnTypeSingle('AP_WD') sm.Get_ReturnTypeSingle('AP_STIG_X') sm.Get_ReturnTypeSingle('AP_STIG_Y')];
-    disp(['final/starting WD: ' num2str(10^6*finalWD(1)) 'um' num2str(finalWD(2)) ' ' num2str(finalWD(3))]);
+    if verbosity
+        disp(['final zeiss/starting WD: ' num2str(10^6*finalWD(1)) 'um' num2str(finalWD(2)) ' ' num2str(finalWD(3))]);
+    end
     z=NaN;
 end
-
 
 end
